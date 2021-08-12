0d3c17b23c55b77f369cc68404529c8fdda10c3b
Jonathas Costa <jonathas.costa@gmail.com>
2021-08-12 19:11:24 +0100

    MonthYear implemented

---
+++ src/SystemExtensions/MonthYear.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace SystemExtensions
+{
+    public struct MonthYear : IComparable<MonthYear>, IEquatable<MonthYear>, IComparable<DateTime>, IEquatable<DateTime>, IFormattable
+    {
+        private const string MonthYearFormat = @"^((?<month>MM).*(?<year>YYYY))|((?<year>YYYY).*(?<month>MM))|((?<month>MM).*(?<year>YY))|((?<year>YY).*(?<month>MM))$";
+
+        public int Month { get; }
+        public int Year { get; }
+
+        public MonthYear(int month, int year)
+            : this()
+        {
+            if (month < 1 || month > 12)
+                throw new ArgumentOutOfRangeException(nameof(month));
+
+            Month = month;
+            Year = year;
+        }
+
+        public DateTime GetDate(int day)
+        {
+            return new DateTime(Year, Month, day);
+        }
+
+        public DateTime GetFirstDay()
+        {
+            return GetDate(1);
+        }
+
+        public DateTime GetLastDay()
+        {
+            return GetDate(1).LastDayOfMonth();
+        }
+
+        public MonthYear AddMonths(int months)
+        {
+            return GetDate(1).AddMonths(months).ToMonthYear();
+        }
+
+        public MonthYear Next()
+        {
+            return AddMonths(1);
+        }
+
+        public MonthYear Previous()
+        {
+            return AddMonths(-1);
+        }
+
+        public int CompareTo(MonthYear other)
+        {
+            return GetHashCode() - other.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return ToString("MM/yyyy", CultureInfo.CurrentCulture);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is MonthYear))
+                return false;
+
+            return this == (MonthYear)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return Year * 12 + Month;
+        }
+
+        public static MonthYear Parse(string strValue)
+        {
+            var parts = strValue.Split('/');
+            if (parts.Length != 2)
+                throw new FormatException();
+
+            var month = int.Parse(parts[0]);
+            var year = int.Parse(parts[1]);
+
+            return new MonthYear(month, year);
+        }
+
+        public static MonthYear Parse(string strValue, string format)
+        {
+            var match = Regex.Match(format, MonthYearFormat, RegexOptions.IgnoreCase);
+            if (!match.Success)
+                throw new FormatException();
+
+            var month = int.Parse(PartialParse(strValue, format, match.Groups["month"].Value));
+            var year = int.Parse(PartialParse(strValue, format, match.Groups["year"].Value));
+
+            return new MonthYear(month, year);
+        }
+
+        private static string PartialParse(string strValue, string format, string part)
+        {
+            return strValue.Substring(format.IndexOf(part), part.Length);
+        }
+
+        public bool Equals(MonthYear other)
+        {
+            return this == other;
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return GetDate(1).ToString(format, formatProvider);
+        }
+
+        public int CompareTo(DateTime other)
+        {
+            return GetHashCode() - other.ToMonthYear().GetHashCode();
+        }
+
+        public bool Equals(DateTime other)
+        {
+            return GetHashCode() == other.ToMonthYear().GetHashCode();
+        }
+
+        #region " Operators "
+
+        public static bool operator ==(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() == b.GetHashCode();
+        }
+
+        public static bool operator !=(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() != b.GetHashCode();
+        }
+
+        public static bool operator <(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() < b.GetHashCode();
+        }
+
+        public static bool operator >(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() > b.GetHashCode();
+        }
+
+        public static bool operator <=(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() <= b.GetHashCode();
+        }
+
+        public static bool operator >=(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() >= b.GetHashCode();
+        }
+
+        public static MonthYear operator ++(MonthYear monthYear)
+        {
+            return monthYear + 1;
+        }
+
+        public static MonthYear operator --(MonthYear monthYear)
+        {
+            return monthYear - 1;
+        }
+
+        public static MonthYear operator +(MonthYear a, int b)
+        {
+            return a.AddMonths(b);
+        }
+
+        public static MonthYear operator -(MonthYear a, int b)
+        {
+            return a.AddMonths(-b);
+        }
+
+        public static int operator -(MonthYear a, MonthYear b)
+        {
+            return a.GetHashCode() - b.GetHashCode();
+        }
+
+        public static implicit operator MonthYear(DateTime date)
+        {
+            return date.ToMonthYear();
+        }
+
+        #endregion
+    }
+}
---
+++ test/SystemExtensions.Tests/DateTimeExtensionsTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Xunit;
+
+namespace SystemExtensions.Tests
+{
+    public class DateTimeExtensionsTest
+    {
+        [Fact]
+        public void FirstDayOfMonthTest()
+        {
+            var date = new DateTime(2000, 1, 31, 23, 59, 59, 999);
+            var expected = new DateTime(2000, 1, 1);
+            var actual = date.FirstDayOfMonth();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LastDayOfMonthTest()
+        {
+            var date = new DateTime(2000, 2, 1);
+            var expected = new DateTime(2000, 2, 29);
+            var actual = date.LastDayOfMonth();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsWeekendTest()
+        {
+            var sunday = new DateTime(2012, 4, 1);
+            Assert.Equal(DayOfWeek.Sunday, sunday.DayOfWeek);
+            Assert.True(sunday.IsWeekend());
+
+            var monday = sunday.AddDays(1);
+            Assert.Equal(DayOfWeek.Monday, monday.DayOfWeek);
+            Assert.False(monday.IsWeekend());
+
+            var tuesday = sunday.AddDays(2);
+            Assert.Equal(DayOfWeek.Tuesday, tuesday.DayOfWeek);
+            Assert.False(tuesday.IsWeekend());
+
+            var wednesday = sunday.AddDays(3);
+            Assert.Equal(DayOfWeek.Wednesday, wednesday.DayOfWeek);
+            Assert.False(wednesday.IsWeekend());
+
+            var thursday = sunday.AddDays(4);
+            Assert.Equal(DayOfWeek.Thursday, thursday.DayOfWeek);
+            Assert.False(thursday.IsWeekend());
+
+            var friday = sunday.AddDays(5);
+            Assert.Equal(DayOfWeek.Friday, friday.DayOfWeek);
+            Assert.False(friday.IsWeekend());
+
+            var saturday = sunday.AddDays(6);
+            Assert.Equal(DayOfWeek.Saturday, saturday.DayOfWeek);
+            Assert.True(saturday.IsWeekend());
+        }
+
+        [Fact]
+        public void FirstMinuteOfDayTest()
+        {
+            var date = new DateTime(2000, 1, 14, 23, 59, 0);
+            var expected = new DateTime(2000, 1, 14);
+            var actual = date.FirstMinuteOfDay();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LastMinuteOfDayTest()
+        {
+            var date = new DateTime(2000, 1, 14);
+            var expected = new DateTime(2000, 1, 14, 23, 59, 0);
+            var actual = date.LastMinuteOfDay();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToMonthYearTest()
+        {
+            var date = new DateTime(2000, 1, 14);
+            var expected = new MonthYear(1, 2000);
+            var actual = date.ToMonthYear();
+            Assert.Equal(expected.Month, actual.Month);
+            Assert.Equal(expected.Year, actual.Year);
+        }
+    }
+}
